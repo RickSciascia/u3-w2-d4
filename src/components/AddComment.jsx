fcddb313b0d41d055cf931c94763c0d57f44980a
Riccardo <richi-97@live.it>
2025-11-27 13:47:34 +0100

    first commit

---
+++ src/components/AddComment.jsx
@@ -0,0 +1,130 @@
+import { useState, useEffect } from "react";
+import { Button, Form } from "react-bootstrap";
+const AddComment = function (props) {
+  // class AddComment extends Component {
+  // state = {
+  //   comment: {
+  //     comment: "",
+  //     rate: 1,
+  //     elementId: this.props.asin,
+  //   },
+  // };
+  const [comment, setComment] = useState({
+    comment: "",
+    rate: 1,
+    elementId: props.asin,
+  });
+
+  useEffect(() => {
+    setComment({
+      ...comment,
+      elementId: props.asin,
+    });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [props.asin]);
+
+  // componentDidUpdate(prevProps) {
+  //   if (prevProps.asin !== this.props.asin) {
+  //     this.setState({
+  //       comment: {
+  //         ...this.state.comment,
+  //         elementId: this.props.asin,
+  //       },
+  //     });
+  //   }
+  // }
+
+  const sendComment = async (e) => {
+    e.preventDefault();
+    try {
+      let response = await fetch(
+        "https://striveschool-api.herokuapp.com/api/comments",
+        {
+          method: "POST",
+          body: JSON.stringify(comment),
+          headers: {
+            "Content-type": "application/json",
+            Authorization:
+              "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2OTBkYmE2MmY0YmQ0NzAwMTU4NWIxZTciLCJpYXQiOjE3NjM2NDUwMTQsImV4cCI6MTc2NDg1NDYxNH0.SVsIVpn0w7k4FvHwlABUadvatF_-fo4jOPhPUI7KnL0",
+          },
+        }
+      );
+      if (response.ok) {
+        alert("Recensione inviata!");
+        // this.setState({
+        //   comment: {
+        //     comment: "",
+        //     rate: 1,
+        //     elementId: this.props.asin,
+        //   },
+        // });
+        setComment({
+          comment: "",
+          rate: 1,
+          elementId: props.asin,
+        });
+      } else {
+        throw new Error("Qualcosa è andato storto");
+      }
+    } catch (error) {
+      alert(error);
+    }
+  };
+
+  return (
+    <div className="my-3">
+      <Form onSubmit={sendComment}>
+        <Form.Group className="mb-2">
+          <Form.Label>Recensione</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Inserisci qui il testo"
+            value={comment.comment}
+            onChange={(e) =>
+              // this.setState({
+              //   comment: {
+              //     ...this.state.comment,
+              //     comment: e.target.value,
+              //   },
+              // })
+              setComment({
+                ...comment,
+                comment: e.target.value,
+              })
+            }
+          />
+        </Form.Group>
+        <Form.Group className="mb-2">
+          <Form.Label>Valutazione</Form.Label>
+          <Form.Control
+            as="select"
+            value={comment.rate}
+            onChange={(e) =>
+              // this.setState({
+              //   comment: {
+              //     ...this.state.comment,
+              //     rate: e.target.value,
+              //   },
+              // })
+              setComment({
+                ...comment,
+                rate: e.target.value,
+              })
+            }
+          >
+            <option>1</option>
+            <option>2</option>
+            <option>3</option>
+            <option>4</option>
+            <option>5</option>
+          </Form.Control>
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Invia
+        </Button>
+      </Form>
+    </div>
+  );
+};
+
+export default AddComment;
---
+++ src/components/SingleComment.jsx
@@ -0,0 +1,40 @@
+import { Button, ListGroup } from "react-bootstrap";
+
+const SingleComment = ({ comment }) => {
+  const deleteComment = async (asin) => {
+    try {
+      let response = await fetch(
+        "https://striveschool-api.herokuapp.com/api/comments/" + asin,
+        {
+          method: "DELETE",
+          headers: {
+            Authorization:
+              "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2OTBkYmE2MmY0YmQ0NzAwMTU4NWIxZTciLCJpYXQiOjE3NjM2NDUwMTQsImV4cCI6MTc2NDg1NDYxNH0.SVsIVpn0w7k4FvHwlABUadvatF_-fo4jOPhPUI7KnL0",
+          },
+        }
+      );
+      if (response.ok) {
+        alert("La recensione è stata elimata!");
+      } else {
+        throw new Error("La recensione non è stata eliminata!");
+      }
+    } catch (error) {
+      alert(error);
+    }
+  };
+
+  return (
+    <ListGroup.Item>
+      {comment.comment}
+      <Button
+        variant="danger"
+        className="ms-2"
+        onClick={() => deleteComment(comment._id)}
+      >
+        Elimina
+      </Button>
+    </ListGroup.Item>
+  );
+};
+
+export default SingleComment;
